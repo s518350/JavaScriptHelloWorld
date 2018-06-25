abe25df81ae1dfbcb78b3b3bb423cdd9ef035d53
s518350 <s518350@nwmissouri.edu>
2018-06-24 20:25:44 -0500

    variable and data type practice

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+"use strict"; //strict mode makes JS work the modern way versus the default way
+
 alert("I'm JavaScript!");
+
+//declaring variables practice
+let admin;
+let name;
+
+name = "John";
+admin = name;
+
+alert(admin);
+
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+
+//data type practice
+let myName = "Amy";
+
+alert(`hello ${1}`);
+alert(`hello ${"myName"}`);
+alert(`hello ${myName}`);
+
